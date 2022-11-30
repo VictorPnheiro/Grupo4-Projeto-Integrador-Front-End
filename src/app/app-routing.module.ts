65b179a3bcbd7057c5f65f2e524a5d29c77ecb3c
Victor Pinheiro <victorpinheiro.dev@gmail.com>
2022-11-30 17:23:10 -0300

    [CRIADO]: Primeiro Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
+import { EstaLogadoGuard } from './shared/guards/esta-logado/esta-logado.guard';
+import { NaoEstaLogadoGuard } from './shared/guards/nao-esta-logado/nao-esta-logado.guard';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./area-logada/area-logada.module').then(
+        (m) => m.AreaLogadaModule
+      ),
+    canActivate: [EstaLogadoGuard],
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
+    canActivate: [NaoEstaLogadoGuard],
+  },
+  {
+    path: '**',
+    component: NaoEncontradoComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AreaLogadaComponent } from './area-logada/area-logada.component';
+import { HomeComponent } from './area-logada/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NaoEncontradoComponent, AreaLogadaComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
