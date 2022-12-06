70fa8e2a5f1a03a77f5b734d13710777865bab48
Victor Pinheiro <victorpinheiro.dev@gmail.com>
2022-12-06 14:56:52 -0300

    [CRIADO]: Componetes de pedidos

---
+++ src/app/area-logada/orders/orders.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrdersRoutingModule } from './orders-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OrdersRoutingModule
+  ]
+})
+export class OrdersModule { }
