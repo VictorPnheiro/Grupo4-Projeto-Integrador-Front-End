7bd13e591549dc762beb769df2bfee755b450e66
Victor Pinheiro <victorpinheiro.dev@gmail.com>
2022-12-06 16:29:05 -0300

    [ATUALIZAÇÃO]: Atualizado serviço de logout

--- src/app/area-logada/area-logada.component.ts
+++ src/app/area-logada/area-logada.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../shared/services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AreaLogadaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
+
+  logout(){
+    this.authService.logout()
+  }
+
 
 }
